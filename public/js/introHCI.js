6e9c945a045f5c1d60239b3dfa5f80279cd599de
Nicholas Wong <nic147919@gmail.com>
2018-02-22 01:36:26 -0800

    Finished lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+
+	$(".likeBtn").click(function() {
+		console.log("asdf");
+		ga('send', 'event', 'like', 'click');
+	});
 }
